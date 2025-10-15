a0dc7c06729d42aa418b4285cd3c1576dbd1bad9
Mahmoud Abdelraouf <mahmoud126125@gmail.com>
2025-10-15 07:33:11 -0400

    chore(docs): add published site to docs/ on docs-branch

---
+++ docs/search/all_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['key_20concepts_0',['Key Concepts',['../md_docs_2pages_2glthreads__guide.html#autotoc_md153',1,'']]],
+  ['keyword_20list_1',['1. Prepare Keyword List',['../md_docs_2pages_2gperf.html#autotoc_md183',1,'']]]
+];
---
+++ docs/search/all_f.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['name_0',['name',['../structdatatype__mapping__.html#ab7555c22ef5c90a910d1b4496e021643',1,'datatype_mapping_::name'],['../structemp__.html#a8b34fd3e29d8ff31803e80969a99421a',1,'emp_::name'],['../structstudent__.html#a0c0c9cd530df1d3b8d34daa9adb12ae8',1,'student_::name']]],
+  ['nature_1',['⚙️ &quot;Generic&quot; Nature',['../md_docs_2pages_2glthreads__guide.html#autotoc_md151',1,'']]],
+  ['navigation_2',['Offset-Based Navigation',['../md_docs_2pages_2glthreads__guide.html#autotoc_md155',1,'']]],
+  ['next_3',['next',['../structvm__page__.html#a87995cd55301183d3d7c94ae9cfc0bfb',1,'vm_page_::next'],['../structvm__page__for__families__.html#a24af31aa2704f20cd44872759337f162',1,'vm_page_for_families_::next'],['../structstudent__.html#a316498c75bc12fa0ccb8c9bb5e7a4492',1,'student_::next']]],
+  ['next_20steps_4',['Next Steps',['../md_docs_2pages_2user__guide.html#autotoc_md223',1,'']]],
+  ['next_5fblock_5',['next_block',['../structblock__meta__data__.html#a05e3a2b0278c7a764f9ee7a904c5883e',1,'block_meta_data_']]],
+  ['next_5fmeta_5fblock_6',['NEXT_META_BLOCK',['../memory__manager_8h.html#ac866fe97df08a8d286eafb8243cd63da',1,'memory_manager.h']]],
+  ['next_5fmeta_5fblock_5fby_5fsize_7',['NEXT_META_BLOCK_BY_SIZE',['../memory__manager_8h.html#a99051f1c2b6af9de76a35dc7546fe60c',1,'memory_manager.h']]],
+  ['nodes_8',['Embedded Nodes',['../md_docs_2pages_2glthreads__guide.html#autotoc_md154',1,'']]],
+  ['null_9',['Check for NULL',['../md_docs_2pages_2user__guide.html#autotoc_md217',1,'']]]
+];
